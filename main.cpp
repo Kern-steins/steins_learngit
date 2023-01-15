af0ec1186f3bafc02323522e8861496abe2573cc
steins <s2453812462@gmail.com>
2023-01-15 17:25:15 +0800

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -5,19 +5,17 @@ void test(int x){
 	int a,b;
 	a = (x%100)/10;
 	b = x % 10;
-	printf ("The ten digit of the number is %d,the one digit of the number is %d",a,b);
+	printf ("The ten digit of the number is %d,the one digit of the number is %d\n",a,b);
 
 }
 int main(){
 	
-	int x;
-	x= 201;
-	int a,b;
-	a = (x%100)/10;
-	b = x%10;
-
-	printf ("The ten digit of the number is %d,the one digit of the number is %d",a,b);
-	test(x);
+	double i = 20.0;
+	double j = i / 7.0;
+	if (j * 7.0 == i)
+			printf ("Equal.\n");
+	else
+			printf ("Unequal.\n");	
 
 	return 0;
 
