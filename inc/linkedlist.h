4f0888822ca7dcbf01513fd64a842d7eb7e5aa2c
steins <s2453812462@gmail.com>
2023-02-08 21:59:01 +0800

    change to doubly linklist

--- inc/linkedlist.h
+++ inc/linkedlist.h
@@ -4,17 +4,18 @@
 typedef struct node* link;
 struct node {
     unsigned char item;
-    link next;
+    link next, pre_v;
 };
 
 link link_make_node(unsigned char item);
 void link_free_node(link p);
 link link_search(unsigned char key);
 void link_insert(link p);
+void link_insert_OD(link p);
 void link_delete(link p);
 void link_traverse(void (*link_fun)(link));
 void link_destroy(void);
-void link_push(link p);
-link link_pop(void);
+void link_FIFO_IN(link p);
+link link_FIFO_OUT(void);
 
 #endif
